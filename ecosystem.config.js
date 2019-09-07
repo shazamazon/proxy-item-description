8d9da4839a4c6c7bd69848a32e4eb0ef6f2ad216
Whitney Lee <whitneyleephotography@gmail.com>
2019-09-07 12:45:27 -0500

    deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-18-223-133-152.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/FEC.pem',
-      ref: 'origin/master',
+      ref: 'master',
       repo: 'https://github.com/shazamazon/proxy-item-description.git',
       path: '/home/ubuntu/proxy-item-description',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
